9dc3f0ff450f107d38fdf78c165bad867e5fe4f3
Ready For Mars <victor.sergio@outlook.com>
2013-04-21 15:22:40 -0600

    Ready For Mars v1
    Ready for mars app for Windows 8

---
+++ ReadyForMars/ReadyForMars/DataModel.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace ReadyForMars
+{
+    public class DataRetriever
+    {
+        private RecordMetadata _lastData;
+        private String _urlTemplate = "http://marsweather.ingenology.com/v1/archive/?page={0}";
+        private Int32 _nextPageNum = 1;
+        private Int32 _currentPage = 1;
+
+        public event EventHandler<DataRetrieverEventArgs> DataRetrieveCompleted;
+
+        public Int32 CurrentPage
+        {
+            get { return _currentPage; }
+        }
+
+        public RecordMetadata LastData
+        {
+            get { return _lastData; }
+        }
+
+        public DataRetriever()
+        {
+            _lastData = null;
+        }
+
+        /// <summary>
+        /// Starts an asynchronous request for the next page only after a call to GetFirstPage.
+        /// </summary>
+        /// <returns>false if there is not another page, or GetFirstPage hasn't been called before</returns>
+        public bool GetNextPage()
+        {
+            if (_lastData != null && _lastData.next != null)
+            {
+                //await UpdateData();
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Starts an asynchronous request for the first page
+        /// </summary>
+        public async Task<RecordMetadata> GetFirstPage()
+        {
+            this._lastData = null;
+            this._nextPageNum = 1;
+            return await UpdateData();
+        }
+
+        async Task<RecordMetadata> UpdateData()
+        {
+            String request = String.Format(this._urlTemplate, this._nextPageNum);
+            
+            // Create an HttpClient instance 
+            HttpClient client = new HttpClient();
+
+            String urlContents = await client.GetStringAsync(request);
+            this._lastData = JsonConvert.DeserializeObject<RecordMetadata>(urlContents);
+            this._currentPage = this._nextPageNum;
+            this._nextPageNum++;
+            //OnRaiseCustomEvent(new DataRetrieverEventArgs(this._lastData));
+
+            return this._lastData;
+
+            /*
+
+            // Send a request asynchronously continue when complete 
+            client.GetAsync(request).ContinueWith(
+                (requestTask) =>
+                {
+                    // Get HTTP response from completed task. 
+                    HttpResponseMessage response = requestTask.Result;
+
+                    // Check that response was successful or throw exception 
+                    //response.EnsureSuccessStatusCode();
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+
+                        // Read response asynchronously as JsonValue and write out top facts for each country 
+                        response.Content.ReadAsStringAsync().ContinueWith(
+                            (task) =>
+                            {
+                                if (task.Exception == null)
+                                {
+                                    if (!task.IsCanceled)
+                                    {
+                                        this._lastData = JsonConvert.DeserializeObject<RecordMetadata>(task.Result);
+                                        this._currentPage = this._nextPageNum;
+                                        this._nextPageNum++;
+                                        OnRaiseCustomEvent(new DataRetrieverEventArgs(this._lastData));
+                                    }
+
+                                }
+                            }
+                        );
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(100);
+                        Console.WriteLine("re-try");
+
+                        this._nextPageNum++;
+                        UpdateData();
+                    }
+                });
+            */
+            //_wc.DownloadDataAsync(new Uri(request));
+        }
+
+        protected virtual void OnRaiseCustomEvent(DataRetrieverEventArgs e)
+        {
+            // Make a temporary copy of the event to avoid possibility of a race condition
+            EventHandler<DataRetrieverEventArgs> handler = DataRetrieveCompleted;
+
+            // Event will be null if there are no subscribers 
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+
+    public class DataRetrieverEventArgs : EventArgs
+    {
+        private RecordMetadata rm;
+
+        public DataRetrieverEventArgs(RecordMetadata rm)
+        {
+            this.rm = rm;
+        }
+
+        public RecordMetadata recordMetadata {
+            get { return this.rm; }
+        }
+    }
+
+    public class Record
+    {
+        public DateTime terrestrial_date;
+        public int sol;
+        public float? ls;
+        public float? min_temp;
+        public float? max_temp;
+        public float? pressure;
+        public String pressure_string;
+        public String abs_humidity;
+        public float? wind_speed;
+        public String wind_direction;
+        public String atmo_opacity;
+        public String season;
+        public String sunrise;
+        public String sunset;
+    }
+    
+    public class RecordMetadata
+    {
+        public int count;
+        public String next;
+        public String previous;
+        public Record[] results;
+    }
+}
---
+++ ReadyForMars/ReadyForMars/GroupedItemsPage.xaml.cs
@@ -0,0 +1,231 @@
+﻿using ReadyForMars.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.ServiceModel.Dispatcher;
+using Windows.UI.Core;
+using Windows.System.Threading;
+using Windows.UI.Popups;
+
+// The Grouped Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234231
+
+namespace ReadyForMars
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class GroupedItemsPage : ReadyForMars.Common.LayoutAwarePage
+    {
+        private DataRetriever retriever;
+
+
+
+
+
+        public GroupedItemsPage()
+        {
+            this.InitializeComponent();
+           this.retriever = new DataRetriever();
+
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+           /*var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
+            this.DefaultViewModel["Groups"] = sampleDataGroups;*/
+
+
+            
+                        
+          
+        }
+
+        /// <summary>
+        /// Invoked when a group header is clicked.
+        /// </summary>
+        /// <param name="sender">The Button used as a group header for the selected group.</param>
+        /// <param name="e">Event data that describes how the click was initiated.</param>
+        void Header_Click(object sender, RoutedEventArgs e)
+        {
+            // Determine what group the Button instance represents
+            var group = (sender as FrameworkElement).DataContext;
+
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            this.Frame.Navigate(typeof(GroupDetailPage), ((SampleDataGroup)group).UniqueId);
+        }
+
+        /// <summary>
+        /// Invoked when an item within a group is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(ItemDetailPage), itemId);
+        }
+
+             /*private async void Page_Loaded(object sender, RoutedEventArgs e)
+              {
+                  await this.retriever.GetFirstPage();
+                  RecordMetadata rd = this.retriever.LastData;
+                  //this.txtStatus.Text = rd.results[0].sol.ToString();
+                  Display_Data(rd.results[0]);
+              }*/
+      
+           private void Display_Data(Record results)
+           {
+                 this.sol_txt.Text = (results.sol.ToString() != null) ? "Sol " + results.sol.ToString() : "N/A";
+                 this.max_value.Text = (results.max_temp.ToString() != null) ? results.max_temp.ToString() : "N/A";
+                 this.min_value.Text = (results.min_temp.ToString() != null) ? results.min_temp.ToString() : "N/A";
+                 this.pressure_value.Text = (results.pressure.ToString() != null) ? results.pressure.ToString() : "N/A";
+                 this.pressure_str.Text = (results.pressure_string.ToString() != null) ? results.pressure_string.ToString() + "Than Nominal" : "N/A";
+                 this.humidity_value.Text = (results.abs_humidity != null) ? results.abs_humidity : "N/A";
+                 this.month.Text = (results.season != null) ? results.season : "N/A";
+                 this.ls_value.Text = (results.ls != null) ? "LS " + results.ls.ToString() : "N/A";
+
+                 if (results.terrestrial_date != null)
+                 {
+                     String[] longDate = results.terrestrial_date.GetDateTimeFormats('D');
+                     this.terrestrial_date.Text = longDate[0].ToString();
+                    
+                 }
+                 else
+                 {
+                     this.terrestrial_date.Text = "N/A";
+                 }
+
+                 if (results.wind_speed != null)
+                 {
+                     float? kmval = results.wind_speed * 3600 / 1000;
+                     this.wind_speed_value.Text = kmval.ToString() + "Km/h";
+                 }
+                 else
+                 {
+                     this.wind_speed_value.Text = "N/A";
+                 }
+
+
+                 if (results.wind_direction != null)
+                 {
+                     if (results.wind_direction != null)
+                     {
+                         if (results.wind_direction == "E")
+                         {
+                             this.wind_dir_value.Text = "East";
+                         }
+                         else if (results.wind_direction == "S")
+                         {
+                             this.wind_dir_value.Text = "South";
+                         }
+                         else if (results.wind_direction == "W")
+                         {
+                             this.wind_dir_value.Text = "West";
+                         }
+                         else
+                         {
+                             this.wind_dir_value.Text = "North";
+                         }
+                     }
+
+                 }
+                 else
+                 {
+                     this.wind_dir_value.Text = "N/A";
+                 }
+
+                 if (results.sunrise != null)
+                 {
+                     String[] date = results.sunrise.Split('T');
+                     DateTime sunrisef = DateTime.ParseExact(date[0], "yyyy-MM-dd", null);
+
+                     string[] hour = date[1].Split('Z');
+                     DateTime sunriseh = DateTime.ParseExact(hour[0], "HH:mm:ss", null);
+
+                     TimeSpan ts = new TimeSpan(sunriseh.Hour, sunriseh.Minute, sunriseh.Second);
+                     sunrisef = sunrisef.Date + ts;
+                     String[] sunriseflongDate = sunrisef.GetDateTimeFormats('g');
+                     this.sunrise_date.Text = sunriseflongDate[0].ToString();
+                 }
+                 else
+                 {
+                     this.sunrise_date.Text = "N/A";
+                 }
+
+
+                 if (results.sunset != null)
+                 {
+                     String[] sunsetdate = results.sunset.Split('T');
+                     DateTime sunsetf = DateTime.ParseExact(sunsetdate[0], "yyyy-MM-dd", null);
+
+                     string[] hour = sunsetdate[1].Split('Z');
+                     DateTime sunseth = DateTime.ParseExact(hour[0], "HH:mm:ss", null);
+
+                     TimeSpan ts = new TimeSpan(sunseth.Hour, sunseth.Minute, sunseth.Second);
+                     sunsetf = sunsetf.Date + ts;
+                     String[] sunsetflongDate = sunsetf.GetDateTimeFormats('g');
+                     this.sunset_date.Text = sunsetflongDate[0].ToString();
+                 }
+                 else
+                 {
+                     this.sunset_date.Text = "N/A";
+                 }
+           }
+
+           async private void pageRoot_Loaded(object sender, RoutedEventArgs e)
+           {
+               await this.retriever.GetFirstPage();
+               RecordMetadata rd = this.retriever.LastData;
+               //this.txtStatus.Text = rd.results[0].sol.ToString();
+               Display_Data(rd.results[0]);
+
+           }
+
+           private void sol_txt_Tapped(object sender, TappedRoutedEventArgs e)
+           {
+               MessageDialog messageDialog = new MessageDialog("Clouds of water ice form high in the Martian air, driven by winds that can gust up to 50 mph on the surface.", "Weather");
+               messageDialog.ShowAsync();
+           }
+
+           private void month_Tapped(object sender, TappedRoutedEventArgs e)
+           {
+               MessageDialog messageDialog = new MessageDialog("Yet coupled with the tenuous air and Mars' daily rotation, that energy produces some remarkable weather.", "Terrestrial Date");
+               messageDialog.ShowAsync();
+           }
+
+           private void ls_value_Tapped(object sender, TappedRoutedEventArgs e)
+           {
+               MessageDialog messageDialog = new MessageDialog("The thin, chill blanket of the Martian atmosphere has an average density less than one-hundredth of the Earth's. At 142 million miles from the Sun, Mars receives less than half the solar energy that reaches the Earth.", "Mars Season");
+               messageDialog.ShowAsync();
+           }
+
+    }
+}
